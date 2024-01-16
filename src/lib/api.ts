48a3e49f4e7b938519202df1d65f30dd6c18496a
Juan Alvarez <juanandres140299@gmail.com>
2024-01-16 18:27:28 -0400

    (chore): add API layer, endpoints, and API route handlers

---
+++ src/lib/api.ts
@@ -0,0 +1,50 @@
+// API Layer to connect to the backend
+// used `fetch` for this layer
+
+import { BASE_URL } from "./endpoints";
+
+export const baseFetch = async (url: string, options: RequestInit = {}) => {
+  return fetch(`${BASE_URL}${url}`, {
+    ...options,
+  });
+};
+
+// *** API Methods ***
+
+export const api = {
+  get: async <T>(url: string, options: RequestInit = {}) => {
+    const response = await baseFetch(url, options);
+    const data: T = await response.json();
+
+    return data;
+  },
+  post: async <T>(url: string, body: any, options: RequestInit = {}) => {
+    const response = await baseFetch(url, {
+      ...options,
+      method: "POST",
+      body: JSON.stringify(body),
+    });
+    const data: T = await response.json();
+
+    return data;
+  },
+  put: async <T>(url: string, body: any, options: RequestInit = {}) => {
+    const response = await baseFetch(url, {
+      ...options,
+      method: "PUT",
+      body: JSON.stringify(body),
+    });
+    const data: T = await response.json();
+
+    return data;
+  },
+  delete: async <T>(url: string, options: RequestInit = {}) => {
+    const response = await baseFetch(url, {
+      ...options,
+      method: "DELETE",
+    });
+    const data: T = await response.json();
+
+    return data;
+  },
+};
---
+++ src/lib/endpoints.ts
@@ -0,0 +1,28 @@
+import { getEnvVariable } from "./env";
+
+export const DOMAIN = getEnvVariable("NEXT_PUBLIC_VERCEL_URL");
+export const BASE_URL = `${DOMAIN}/api`;
+
+function prefixEndpoint(endpoint: string) {
+  return `${BASE_URL}${endpoint}`;
+}
+
+export const endpoints = {
+  polls: {
+    all: prefixEndpoint("/polls"),
+    single: (id: string) => prefixEndpoint(`/polls/${id}`),
+    create: prefixEndpoint("/polls"),
+    update: (id: string) => prefixEndpoint(`/polls/${id}`),
+    delete: (id: string) => prefixEndpoint(`/polls/${id}`),
+    archive: (id: string) => prefixEndpoint(`/polls/${id}/archive`),
+  },
+  pollsOption: {
+    create: prefixEndpoint("/polls/option"),
+    update: (id: string) => prefixEndpoint(`/polls/option/${id}`),
+    delete: (id: string) => prefixEndpoint(`/polls/option/${id}`),
+  },
+  pollsVote: {
+    vote: prefixEndpoint("/polls/vote"),
+    votes: (pollId: string) => prefixEndpoint(`/polls/vote/${pollId}`),
+  },
+};
