1aa6daa8463a968326e583b71ab193c139e944e4
Juan Alvarez <juanandres140299@gmail.com>
2024-02-10 10:20:22 -0400

    feat: add toggle to results page

--- src/components/VotesChart.tsx
+++ src/components/VotesChart.tsx
@@ -1,9 +1,20 @@
 "use client";
 
 import { Prisma } from "@prisma/client";
+import { BarChartIcon, PieChartIcon } from "@radix-ui/react-icons";
 import { useChannel } from "ably/react";
 import React from "react";
-import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
+import {
+  Bar,
+  BarChart,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from "recharts";
+import colors from "tailwindcss/colors";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
 
 type PollOption = Prisma.PollOptionGetPayload<{
   include: { votes: true };
@@ -14,7 +25,35 @@ type Props = {
   pollId: string;
 };
 
+const mockData = [
+  {
+    name: "Group A",
+    value: 400,
+  },
+  {
+    name: "Group B",
+    value: 300,
+  },
+  {
+    name: "Group C",
+    value: 300,
+  },
+  {
+    name: "Group D",
+    value: 200,
+  },
+  {
+    name: "Group E",
+    value: 278,
+  },
+  {
+    name: "Group F",
+    value: 189,
+  },
+];
+
 export default function VotesChart({ options, pollId }: Props) {
+  const [chartType, setChartType] = React.useState<"bar" | "pie">("bar");
   const [votes, setVotes] = React.useState(() => {
     return options.map((option) => ({
       ...option,
@@ -41,16 +80,60 @@ export default function VotesChart({ options, pollId }: Props) {
     [votes],
   );
 
-  return (
-    <div className="h-[50dvh] w-full">
-      <ResponsiveContainer width="100%">
-        <BarChart data={data}>
-          <YAxis hide dataKey="value" />
-          <XAxis dataKey="name" />
+  function handleChartTypeChange(value: "bar" | "pie") {
+    return () => setChartType(value);
+  }
 
-          <Bar fill="#f59e0b" dataKey="value" />
-        </BarChart>
-      </ResponsiveContainer>
-    </div>
+  return (
+    <Tabs value={chartType}>
+      <TabsContent value="pie" className="h-[50dvh] w-full">
+        <ResponsiveContainer width="100%">
+          <PieChart>
+            <Pie
+              stroke={colors.gray[950]}
+              data={mockData}
+              dataKey="value"
+              nameKey="name"
+              cx="50%"
+              cy="50%"
+              outerRadius="90%"
+              fill="#f59e0b"
+            />
+          </PieChart>
+        </ResponsiveContainer>
+      </TabsContent>
+
+      <TabsContent value="bar" className="h-[50dvh] w-full">
+        <ResponsiveContainer width="100%">
+          <BarChart data={mockData}>
+            <YAxis hide dataKey="value" />
+            <XAxis dataKey="name" />
+
+            <Bar fill="#f59e0b" dataKey="value" />
+          </BarChart>
+        </ResponsiveContainer>
+      </TabsContent>
+
+      <div className="mt-8 grid w-full justify-center">
+        <TabsList>
+          <TabsTrigger
+            className="w-40"
+            value="bar"
+            onClick={handleChartTypeChange("bar")}
+          >
+            <BarChartIcon />
+            Bar
+          </TabsTrigger>
+          <TabsTrigger
+            className="w-40"
+            value="pie"
+            onClick={handleChartTypeChange("pie")}
+          >
+            <PieChartIcon />
+            Pie
+          </TabsTrigger>
+        </TabsList>
+      </div>
+    </Tabs>
   );
 }
